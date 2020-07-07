60fc6483cf5761c16d36a21a4088241c969b3de5
Pablo Emilio Escobar Gaviria <pablo-escobar@riseup.net>
2020-07-07 14:54:01 -0300

    Refactored the install script

--- install.sh
+++ install.sh
@@ -1,62 +1,63 @@
+#!/bin/sh
+
+puts ()
+{
+    printf "\033[1m\033[38;5;%sm[%s]\033[m %s\n\n" "$3" "$1" "$2"
+}
+
 error ()
 {
-    echo "ERROR: $1"
+    puts ERROR "$1" 9
     exit 1
 }
 
-dependecyError () 
+dependencyWarning () 
 {
-    echo "ERROR: The command '$1' could not be found."
-    echo "Please make sure it is included in the \$PATH enviroment variable or follow the installation instructions on $2."
-    exit 1
+    puts WARNING "$(printf "The command '%s' could not be found.\nPlease make sure it is included in the \$PATH enviroment variable or follow the installation instructions on %s." "$1" "$2")" 3
 }
 
-if [ "$(id -u)" -ne "0" ]; then
+message ()
+{
+    puts INSTALLER "$1" 2
+}
+
+if [ "$(id -u)" -ne "0" ]
+then
     error "This script requires administrator-privileges"
 fi
 
-if ! [ -x "$(command -v xelatex)" ]; then
-    dependencyError "xelatex" "https://tug.org/texlive/"
+if ! [ -x "$(command -v xelatex)" ]
+then
+    dependencyWarning "xelatex" "https://tug.org/texlive/"
 fi
 
-if ! [ -x "$(command -v pdf2svg)" ]; then
-    dependencyError "pdf2svg" "https://github.com/dawbarton/pdf2svg"
+if ! [ -x "$(command -v pdf2svg)" ]
+then
+    dependencyWarning "pdf2svg" "https://github.com/dawbarton/pdf2svg"
 fi
 
-printf "INSTALLING tikztosvg(1):\n\n"
+message "Installing tikztosvg(1)"
 
-if [ -f $HOME/.local/bin/tikztosvg ]; then
-    rm $HOME/.local/bin/tikztosvg
+if [ -f "$HOME/.local/bin/tikztosvg" ]
+then
+    rm "$HOME/.local/bin/tikztosvg"
 fi
 
-wget https://raw.githubusercontent.com/GarkGarcia/tikztosvg/master/tikztosvg -P $HOME/.local/bin/
+wget https://raw.githubusercontent.com/GarkGarcia/tikztosvg/master/tikztosvg -P "$HOME/.local/bin/"\
+    && chmod +x "$HOME/.local/bin/tikztosvg"\
+    || exit 1
 
-if [ "$?" -ne "0" ]; then
-    exit 1
+tmp="$(mktemp -d)"
+message "Installing manual entry for tikztosvg(1)"
+wget https://raw.githubusercontent.com/GarkGarcia/tikztosvg/master/man/tikztosvg.1 -P "$tmp"\
+    && install -g 0 -o 0 -m 0644 "$tmp/tikztosvg.1" "$HOME/.local/share/man/man1/"\
+    || exit 1
+
+if [ -f "$HOME/.local/share/man/man1/tikztosvg.1.gz" ]
+then
+    rm "$HOME/.local/share/man/man1/tikztosvg.1.gz"
 fi
 
-chmod +x $HOME/.local/bin/tikztosvg
-
-man "tikztosvg" > /dev/null 2>&1
-if [ "$?" -ne "0" ]; then
-    tmp="$(mktemp -d)"
-    printf "INSTALLING MANUAL ENTRY FOR tikztosvg(1):\n\n"
-    wget https://raw.githubusercontent.com/GarkGarcia/tikztosvg/master/man/tikztosvg.1 -P "$tmp"
-    install -g 0 -o 0 -m 0644 "$tmp/tikztosvg.1" $HOME/.local/share/man/man1/
-    
-    if [ "$?" -ne "0" ]; then
-        exit 1
-    fi
-
-    if [ -f $HOME/.local/share/man/man1/tikztosvg.1.gz ]; then
-        rm $HOME/.local/share/man/man1/tikztosvg.1.gz
-    fi
-
-        gzip $HOME/.local/share/man/man1/tikztosvg.1
-    rm "$tmp" -r
-    
-    if [ "$?" -ne "0" ]; then
-        exit 1
-    fi
-fi
+gzip "$HOME/.local/share/man/man1/tikztosvg.1"
+rm "$tmp" -r
 
